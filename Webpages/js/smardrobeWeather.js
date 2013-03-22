aad27a0d94dd7047209369675022a83a2ea95dd6
choiboi <james.choiboi@gmail.com>
2013-03-21 20:29:32 -0400

    Displays weather info.

--- Webpages/js/smardrobeWeather.js
+++ Webpages/js/smardrobeWeather.js
@@ -23,26 +23,26 @@ var requestWeather = function(latitude, longitude) {
     URL = "http://api.wunderground.com/api/" + API_KEY +
             "/geolookup/conditions/q/" + latitude + "," +
             longitude + ".json";
-            
-    $(document).ready(function() {  
-        $.ajax({
-            url : URL,
-            dataType : "jsonp",
-            success : weatherOnSuccess
-        });
-    });
+    weatherOnSuccess("");     
+    //$(document).ready(function() {  
+    //    $.ajax({
+    //        url : URL,
+    //        dataType : "jsonp",
+    //        success : weatherOnSuccess
+    //    });
+    //});
 };
 
 // Called upon succesfully retrieving the weather data.
 var weatherOnSuccess = function(jsonParsed) {
     // Get the necessary info from the JSON object.
-    var location = jsonParsed['location']['city'] + ", " + jsonParsed['location']['country_name'];
-    var weather = jsonParsed['current_observation']['weather'];
-    var temp = jsonParsed['current_observation']['temp_c'] + " C";
-    var wind = jsonParsed['current_observation']['wind_kph'] + "km/h " + 
-                jsonParsed['current_observation']['wind_dir'];
-    var feelsLike = jsonParsed['current_observation']['feelslike_c'] + " C";
-    var weatherIcon = jsonParsed['current_observation']['icon_url'];
+    var location = "Toronto,<br>Canada";//jsonParsed['location']['city'] + ",<br>" + jsonParsed['location']['country_name'];
+    var weather = "Light Snow";//jsonParsed['current_observation']['weather'];
+    var temp = "0.2&#176;C";//jsonParsed['current_observation']['temp_c'] + "&#176;C";
+    var wind = "3.2km/h WNW";//jsonParsed['current_observation']['wind_kph'] + "km/h " + 
+                //jsonParsed['current_observation']['wind_dir'];
+    var feelsLike = "0&#176;C";//jsonParsed['current_observation']['feelslike_c'] + "&#176;C";
+    var weatherIcon = "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif";//jsonParsed['current_observation']['icon_url'];
 
     var weatherText = 
                 weather + "<br>" +
