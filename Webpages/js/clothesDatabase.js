296d102b2a0ee9798f359d461d72ae365d341b9c
choiboi <james.choiboi@gmail.com>
2013-03-25 12:58:00 -0400

    New DB js file.

---
+++ Webpages/js/clothesDatabase.js
@@ -0,0 +1,33 @@
+/*
+ * 
+ * This javascript handles the tasks related to maintaining
+ * the database, retrieving and deleting clothes in the
+ * database.
+ *
+ */
+
+ $(document).ready(function() {
+ 	db = new Database();
+ });
+
+
+function Database() {
+
+
+	this.createDB = function() {
+
+	};
+
+	this.getClothing = function(clothingID) {
+
+	};
+
+	this.deleteClothing = function(clothingID) {
+
+	};
+
+	this.increaseDBSize = function() {
+
+	};
+}
+var db;
