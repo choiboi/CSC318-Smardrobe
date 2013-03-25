2a0b1cf0a42c080fc2f3eb1f4550dd593b40d345
choiboi <james.choiboi@gmail.com>
2013-03-25 18:08:43 -0400

    Delete implemented.

--- Webpages/js/clothesDatabase.js
+++ Webpages/js/clothesDatabase.js
@@ -6,35 +6,56 @@
  *
  */
 
- $(document).ready(function() {
+//
+// This function will store new clothing image into the
+// database.
+//
+// @param nameID - Unique name of the clothing.
+// @param clothing - image blob.
+//
+var addNewClothing = function(nameID, clothing) {
+	db.addClothing(nameID, clothing);
+};
+
+//
+// This function will retrieve the specified clothing image
+// with the given nameID and make a callback with that image.
+//
+// @param nameID - Unique name of the clothing.
+// @param cb - Callback function.
+//
+var getClothing = function(nameID, cb) {
+	db.getClothing(nameID, cb);
+};
+
+//
+// This function will delete the specified clothing.
+//
+// @param nameID - Unique name of the clothing.
+//
+var deleteClothing = function(nameID) {
+	db.deleteClothing(nameID);
+};
+
+
+$(document).ready(function() {
  	var db = new Database();
  	db.openDB();
-
- 	db.addClothing('asd1123', "23");
- 	db.getClothing('asd1', t);
- });
-
- var t = function(r) {
- 	alert(r);
- };
-
+});
 
 function Database() {
-
 
 	var dbName = "CLOTHES";
 	var dbVersion = "1.0";
 	var dbDispName = "Smardrobe Clothes";
 	// Allocate 10MB for database.
 	var dbSize = 1024 * 1024 * 10;
-	var dbCurrSize = 0;
 	var db;
 
 	this.openDB = function() {
 		db = window.openDatabase("Database", dbVersion, dbDispName, dbSize);
 		db.transaction(function(tx) {
 			tx.executeSql("CREATE TABLE IF NOT EXISTS " + dbName + " (id unique, clothing, fileSize)");
-			dbCurrSize = ;
 		});
 	};
 
@@ -46,29 +67,35 @@ function Database() {
 
 	this.addClothing = function(ID, clothing) {
 		var size = (clothing.length * 4).toString();
+		var query = "INSERT INTO " + dbName + " (id, clothing, fileSize) VALUES ('" + ID + "', '" + 
+						clothing + "', '" + size + "')";
 
 		db.transaction(function(tx) {
-			tx.executeSql("INSERT INTO " + dbName + " (id, clothing, fileSize) VALUES ('" + ID + "', '" + 
-								clothing + "', '" + size + "')");
+			tx.executeSql(query);
 		}, errorCB);
 	};
 
 	this.getClothing = function(ID, cb) {
+		var query = "SELECT * FROM " + dbName + " WHERE id='" + ID + "'";
 		db.transaction(function(tx) {
-			tx.executeSql("SELECT * FROM " + dbName + " WHERE id='" + ID + "'", [], 
+			tx.executeSql(query, [], 
 				function(tx, result){
 					cb(result.rows.item(0).clothing);
 				}, errorCB);
 		}, errorCB);
 	};
 
-	this.deleteClothing = function(clothingID) {
+	this.deleteClothing = function(ID) {
+		var query = "DELETE FROM " + dbName + " WHERE id='" + ID + "'";
 
+		db.transaction(function(tx) {
+			tx.executeSql(query);
+		}, errorCB);
 	};
 
-	this.increaseDBSize = function() {
+	// this.increaseDBSize = function() {
 
-	};
+	// };
 
 	var errorCB = function(err) {
 		alert("Error processing SQL: " + err.code);
