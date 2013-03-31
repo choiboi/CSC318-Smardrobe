046d9f305f936c016e065fc1fc1572f079fc4b9d
choiboi <james.choiboi@gmail.com>
2013-03-31 02:22:02 -0400

    Got document ready working.

--- Webpages/js/addClothing.js
+++ Webpages/js/addClothing.js
@@ -1,26 +1,27 @@
 var initialize = function() {
 	//startCameraInfo();
+	$('#info').popup('open');
 
-	if (typeof localStorage.info === "undefined") {
-		localStorage.info = false;
-		$('#info').popup('open');
-		//$('#popupBut').click();
-	} else if (localStorage.info === "false") {
-		$('#info').popup('open');
-		//$('#popupBut').click();
-	}
+	// if (typeof localStorage.info === "undefined") {
+	// 	localStorage.info = false;
+	// 	$('#info').popup('open');
+	// 	$('#popupBut').click();
+	// } else if (localStorage.info === "false") {
+	// 	$('#info').popup('open');
+	// 	$('#popupBut').click();
+	// }
 }
 
-var startCameraInfo = function() {
-	navigator.camera.getPicture(this.cameraSuccess, this.cameraError);
-};
+// var startCameraInfo = function() {
+// 	navigator.camera.getPicture(this.cameraSuccess, this.cameraError);
+// };
 
-var cameraSuccess = function(imgData) {
+// var cameraSuccess = function(imgData) {
 
-};
+// };
 
-var cameraError = function(msg) {
-	alert("Camera Failed: " + msg);
-};
+// var cameraError = function(msg) {
+// 	alert("Camera Failed: " + msg);
+// };
 
 $(document).ready(initialize);
