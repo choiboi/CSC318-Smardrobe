5ba27e86e504a3f8650008da6fdfba25456696bb
choiboi <james.choiboi@gmail.com>
2013-03-31 04:30:48 -0400

    Info dialog functioning.

--- Webpages/js/addClothing.js
+++ Webpages/js/addClothing.js
@@ -2,7 +2,7 @@ var initialize = function() {
 	//startCameraInfo();
 
 	if (typeof localStorage.info === "undefined") {
-		localStorage.info = false;
+		localStorage.info = "false";
 		$('#info').popup('open');
 	} else if (localStorage.info === "false") {
 		$('#info').popup('open');
@@ -20,5 +20,15 @@ var initialize = function() {
 // var cameraError = function(msg) {
 // 	alert("Camera Failed: " + msg);
 // };
+
+var dialogOK = function() {
+	var t = $('#checkbox-1').is(':checked');
+	if (t === true) {
+		localStorage.info = "true";
+	}
+
+	$('#info').popup({history : false});
+	$('#info').popup('close');
+};
 
 $(document).ready(initialize);
