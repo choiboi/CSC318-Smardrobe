c57d84a5d25382e2c1ac3360a4a8823aceabcdb3
Xiax <xiax.dong@mail.utoronto.ca>
2013-04-01 16:55:47 -0400

    Updated delete button.

--- Webpages/js/wardrobeLoader.js
+++ Webpages/js/wardrobeLoader.js
@@ -7,12 +7,19 @@ var multiClothes = [];
 var images = [];
 var displayLimit = 3;
 var x_start;
+var clothingNames = [];
 
+$(document).ready(function() {
+    document.getElementById('mainBody').on('swipeleft', function() {
+        loadPreviousPage();
+    });
+    document.getElementById('mainBody').on('swiperight', function() {
+        loadNextPage();
+    });
+    
+    
+});
 
-function onload() {
-    document.getElementById('main').addEventListener('touchstart', touchStart(event), false);
-    document.getElementById('main').addEventListener('touchend', touchEnd(event), false);
-}
 
 function touchStart(e) {
     var touch = e.touches[0];
@@ -41,6 +48,7 @@ function loadPreviousPage() {
 function loadNextPage() {
     if (pageNumber < maxPages) {
         pageNumber += 1;
+        alert("page detected");
         $.mobile.changePage($("#page" + pageNumber), {
             transition: "slide", 
             reverse: false
@@ -54,7 +62,86 @@ var getClothingSuccess = function(clothing) {
 
 var deleteFunction = function(event) {
     event.stopImmediatePropagation();
-    event.preventDefault();    
+    event.preventDefault();
+    
+    if (multiSelect) {
+        multiClothes.sort(function(a,b){return a-b});
+        for (var i = 0; i < multiClothes.length; i++) {
+            deleteClothing(clothingNames[multiClothes[i] - i]);
+            deleteClothingDB(clothingNames[multiClothes[i] - i]);
+            images.splice(multiClothes[i] - i, 1);
+            clothingNames.splice(multiClothes[i] - i, 1);
+        }
+        
+        if (multiClothes.length > 0) {
+            for (i = 0; i < maxPages; i++) {
+                $("#grid" + i).remove();
+            }
+            
+            var counter = 0;
+            var hasNext;
+
+            if (images.length > 0) {
+                hasNext = true;
+            } else {
+                hasNext = false;
+            }
+
+            while (hasNext) {
+                $("#body" + counter).append('<div class="ui-grid-b" id="grid' + counter + '"></div>');
+                hasNext = imageListDisplay(counter);
+                counter += 1;
+            }
+            
+            maxPages = counter;
+            
+            if (pageNumber > maxPages - 1) {
+                pageNumber = maxPages - 1;
+                $.mobile.changePage($("#page" + pageNumber), {
+                    transition: "slide", 
+                    reverse: false
+                });
+            }
+        }
+
+        
+    } else {
+        deleteClothing(clothingNames[selectedImage]);
+        deleteClothingDB(clothingNames[selectedImage]);
+        images.splice(selectedImage, 1);
+        clothingNames.splice(selectedImage, 1);
+        
+        for (i = pageNumber; i < maxPages; i++) {
+            $("#grid" + i).remove();
+        }
+            
+        counter = pageNumber;
+
+        if (images.length > (counter + 1) * displayLimit) {
+            hasNext = true;
+        } else {
+            hasNext = false;
+        }
+
+        while (hasNext) {
+            $("#body" + counter).append('<div class="ui-grid-b" id="grid' + counter + '"></div>');
+            hasNext = imageListDisplay(counter);
+            counter += 1;
+        }
+            
+        maxPages = counter;
+        
+        if (pageNumber > maxPages - 1) {
+            pageNumber = maxPages - 1;
+            $.mobile.changePage($("#page" + pageNumber), {
+                transition: "slide", 
+                reverse: false
+            });
+        }
+    }
+    
+    multiSelect = false;
+    
 }
 
 //done
@@ -65,17 +152,22 @@ var cancelFunction = function(event) {
     multiSelect = false;
     $("#delete").remove();
     $("#cancel").remove();
-    $(".footer").append('<a href="wardrobe.html" data-role="button" id="multiSelect" rel="external" onclick="multiSelect(); return false">Multi Select</a>');
+    $(".footer").append('<a href="wardrobe.html" data-role="button" id="multiSelect" rel="external" onclick="multiSelectFuntion(); return false">Multi Select</a>');
     
-    $("#multiSelect").bind('tap', multiSelectFunction(event));
+    $("#multiSelect").bind('tap', function(event) {
+        multiSelectFunction(event);
+    });
+    
+    $(".footer").trigger("create");
 }
 
 var imageSelectFunction = function(event) {
     event.stopImmediatePropagation();
     event.preventDefault();
     
+    selectedImage = Number(event.currentTarget.id);
     if (multiSelect) {
-        multiSelect.push(Number(event.currentTarget.id));
+        multiClothes.push(Number(event.currentTarget.id));
         $("#" + event.currentTarget.id).css("opacity", 0.5);
     } else {
         zoomIn(Number(event.currentTarget.id));
@@ -88,12 +180,18 @@ var multiSelectFunction = function(event) {
     event.preventDefault();
         
     $("#multiSelect").remove();
-    $(".footer").append('<a href="wardrobe.html" data-role="button" id="delete" rel="external" onclick="delete(); return false">Delete</a>');
-    $(".footer").append('<a href="wardrobe.html" data-role="button" id="cancel" rel="external" onclick="cancel(); return false">Cancel</a>');
-    $("#delete").bind('tap', deleteFunction(event));
-    $("#cancel").bind('tap', cancelFunction(event));
-    multiSelect = true;
+    $(".footer").append('<a href="#" data-role="button" id="delete" rel="external" >Delete</a>');
+    $(".footer").append('<a href="#" data-role="button" id="cancel" rel="external" >Cancel</a>');
+    $("#delete").bind('tap', function(event) {
+        deleteFunction(event);
+    });
+    $("#cancel").bind('tap', function(event) {
+        cancelFunction(event);
+    });
     
+    multiSelect = true;
+    $(".footer").trigger("create");
+    return false;
 }
 
 function initializer(image, disLimit) {
@@ -101,11 +199,10 @@ function initializer(image, disLimit) {
     selectedImage = image;
     filterOptions = [];
     filterOptions.push('shoe');
-    
-    var clothingNames = [];
 
+    
     for (var i = 0; i < filterOptions.length; i++) {
-        clothingNames.concat(retrieveClothingByTag(filterOptions[i]));
+        clothingNames = clothingNames.concat(retrieveClothingByTag(filterOptions[i]));
     }
 
     for (i = 0; i < clothingNames.length; i++) {
@@ -130,22 +227,40 @@ function initializer(image, disLimit) {
         $("#page" + counter).append('<div data-role="footer" data-position="fixed" data-theme="a" class="footer"></div>');
         counter += 1;
     }
+    
+    //adding image holder page for zoomed in images
+
 
     maxPages = counter;
     $(".header").append('<a href="smardrobe.html" data-role="button" id="backButton">Back</a>');
     $(".header").append('<h4>Wardrobe</h4>');
     $(".header").append('<a href="#popupMenu" data-rel="popup" data-role="button" data-inline="true">Filter</a>');
-    $(".footer").append('<a href="wardrobe.html" data-role="button" id="multiSelect" rel="external" onclick="multiSelect(); return false">Multi Select</a>');
+    $(".footer").append('<a href="#" data-role="button" id="multiSelect">Multi Select</a>');
     
-    $("#multiSelect").bind('tap', multiSelectFunction(event));
-    $(".image").bind('tap', imageSelectFunction(event));
+    $("#multiSelect").bind('tap click', function(event) {
+        return multiSelectFunction(event);
+    });
+    $(".image").bind('tap click', function(event) {
+        imageSelectFunction(event);
+    });
+    
+    $("#mainBody").append('<div data-role="page" id="pageImage" data-theme="a"></div>');
+    $("#pageImage").append('<div data-role="header" data-theme="a" data-id="wardrobeHeader" data-position="fixed" id="imageHeader" data-add-back-btn="true"></div>');
+    $("#pageImage").append('<div data-role="content" id="body' + counter + '"></div>');
+    $("#bodyImage").append('<div class="ui-grid-b" id="grid' + counter + '"></div>');
+    $("#pageImage").append('<div data-role="footer" data-position="fixed" data-theme="a" id="imageFooter"></div>');
+    $("#imageFooter").append('<a href="#" data-role="button" id="imageDelete">Delete</a>');
+    $("#imageDelete").bind('tap', function(event) {
+        deleteFunction(event);
+    });
+    
 }
 
 function imageListDisplay (counter) {
     var gridContents = "";
     var letter = "a";
 
-    for (var i = counter * displayLimit; i < (counter + 1) * displayLimit - 1; i++) {
+    for (var i = counter * displayLimit; i < (counter + 1) * displayLimit; i++) {
         if (i < images.length) {
             if (i % 3 == 0) {
                 letter = "a";
@@ -159,7 +274,7 @@ function imageListDisplay (counter) {
             var image = document.createElement("img");
 
             image.src = "data:image/png;base64," + images[i];
-            image.setAttribute('onclick', "zoomIn(" + i + ")");
+            //image.setAttribute('onclick', "zoomIn(" + i + ")");
             image.setAttribute('class', "image");
             image.setAttribute('id', "" + i);
             
