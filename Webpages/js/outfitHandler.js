bab8a9b30971e6beedec4c27020eb1802c52507f
choiboi <james.choiboi@gmail.com>
2013-04-01 17:02:15 -0400

    Swipe clothes workinggit status

--- Webpages/js/outfitHandler.js
+++ Webpages/js/outfitHandler.js
@@ -2,6 +2,10 @@ var clothesTop;
 var clothesBottom;
 var clothesShoe;
 
+var currTop;
+var currBottom;
+var currShoe;
+
 var topCategories = ['outerwear', 'tshirt'];
 var bottomCategories = ['pant'];
 var shoeCategories = ['shoe'];
@@ -31,19 +35,73 @@ var getListOfShoes = function() {
 };
 
 var setupOutfit = function() {
-	var topwear = clothesTop[0];
-	var bottomwear = clothesBottom[0];
-	var shoewear = clothesShoe[0];
+	currTop = clothesTop[0];
+	currBottom = clothesBottom[0];
+	currShoe = clothesShoe[0];
 
-	getClothingDB(topwear, function(c) {
+	getClothingDB(currTop, function(c) {
 		$('#topImg').attr('src', c);
 	});
 
-	getClothingDB(bottomwear, function(c) {
+	getClothingDB(currBottom, function(c) {
 		$('#bottomImg').attr('src', c);
 	});
 
-	getClothingDB(shoewear, function(c) {
+	getClothingDB(currShoe, function(c) {
+		$('#shoeImg').attr('src', c);
+	});
+};
+
+var swipedTop = function(direction) {
+	var currInd = clothesTop.indexOf(currTop);
+	var nextInd = 0;
+
+	if (currInd + direction < 0) {
+		nextInd = clothesTop.length - 1;
+	} else if (currInd + direction >= clothesTop.length) {
+		nextInd = 0;
+	} else {
+		nextInd = currInd + direction;
+	}
+
+	currTop = clothesTop[nextInd];
+	getClothingDB(currTop, function(c) {
+		$('#topImg').attr('src', c);
+	});
+};
+
+var swipedBottom = function(direction) {
+	var currInd = clothesBottom.indexOf(currBottom);
+	var nextInd = 0;
+
+	if (currInd + direction < 0) {
+		nextInd = clothesBottom.length - 1;
+	} else if (currInd + direction >= clothesBottom.length) {
+		nextInd = 0;
+	} else {
+		nextInd = currInd + direction;
+	}
+
+	currBottom = clothesBottom[nextInd];
+	getClothingDB(currBottom, function(c) {
+		$('#bottomImg').attr('src', c);
+	});
+};
+
+var swipedShoe = function(direction) {
+	var currInd = clothesShoe.indexOf(currShoe);
+	var nextInd = 0;
+
+	if (currInd + direction < 0) {
+		nextInd = clothesShoe.length - 1;
+	} else if (currInd + direction >= clothesShoe.length) {
+		nextInd = 0;
+	} else {
+		nextInd = currInd + direction;
+	}
+
+	currShoe = clothesShoe[nextInd];
+	getClothingDB(currShoe, function(c) {
 		$('#shoeImg').attr('src', c);
 	});
 };
@@ -52,29 +110,29 @@ var setupOutfit = function() {
 $(document).ready(function() {
 	// Swipe event listener for top wear.
 	$('#top').on('swipeleft', function() {
-		alert('top: swipe left');
+		swipedTop(-1);
 	});
 
 	$('#top').on('swiperight', function() {
-		alert('top: swipe right');
+		swipedTop(1);
 	});
 
 	// Swipe event listener for pants.
 	$('#bottom').on('swipeleft', function() {
-		alert('bottom: swipe left');
+		swipedBottom(-1);
 	});
 
 	$('#bottom').on('swiperight', function() {
-		alert('bottom: swipe right');
+		swipedBottom(1);
 	});
 
 	// Swipe event listener for shoes.
 	$('#shoe').on('swipeleft', function() {
-		alert('shoe: swipe left');
+		swipedShoe(-1);
 	});
 
 	$('#shoe').on('swiperight', function() {
-		alert('shoe: swipe right');
+		swipedShoe(1);
 	});
 
 	getListOfTop();
